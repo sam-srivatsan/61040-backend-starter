63cc089066d0db6f3bdec811c23c7c11cd7afe1c
Sam Vinu-Srivatsan <sam.vinu-srivatsan.ext@afresearchlab.com>
2024-10-28 00:08:13 -0400

    equals check

--- server/concepts/grouping.ts
+++ server/concepts/grouping.ts
@@ -36,21 +36,37 @@ export default class GroupingConcept {
 
   /**
    * Invites a user to join the group if the inviter is the admin.
-   */
+  //  */
+  // async inviteUser(groupId: ObjectId, invitee: ObjectId) {
+  //   const group = await this.groups.readOne({ _id: groupId });
+  //   if (!group) throw new NotFoundError("Group not found!");
+
+  //   if (group.members.includes(invitee)) {
+  //     return { msg: "User is already a member!" };
+  //   }
+
+  //   // Add invitee to the group members
+  //   group.members.push(invitee);
+
+  //   await this.groups.partialUpdateOne({ _id: groupId }, { members: group.members });
+  //   return { msg: "User successfully invited!" };
+  // }
   async inviteUser(groupId: ObjectId, invitee: ObjectId) {
     const group = await this.groups.readOne({ _id: groupId });
     if (!group) throw new NotFoundError("Group not found!");
 
-    if (group.members.includes(invitee)) {
-      return { msg: "User is already a member!" };
+    // Check if the invitee is already a member using .equals() on ObjectId
+    const isAlreadyMember = group.members.some(member => member.equals(invitee));
+    if (isAlreadyMember) {
+        return { msg: "User is already a member!" };
     }
 
-    // Add invitee to the group members
+    // Add the invitee to members if not already present
     group.members.push(invitee);
-
     await this.groups.partialUpdateOne({ _id: groupId }, { members: group.members });
-    return { msg: "User successfully invited!" };
-  }
+    return { msg: "User has been successfully invited!" };
+}
+
 
   /**
    * Lists all members of the group.
@@ -62,31 +78,61 @@ export default class GroupingConcept {
     return group.members;
   }
 
+  // async assertIsInGroup(userId: ObjectId, groupId: ObjectId) {
+  //   const group = await this.groups.readOne({ _id: groupId });
+  //   if (!group) {
+  //     throw new NotFoundError(`Group ${groupId} does not exist!`);
+  //   }
+  //   if (!group.members.includes(userId) ) {
+  //     throw new NotAllowedError(`User ${userId}, not allowed to invite to ${groupId}`);
+  //   }
+  // }
   async assertIsInGroup(userId: ObjectId, groupId: ObjectId) {
     const group = await this.groups.readOne({ _id: groupId });
     if (!group) {
-      throw new NotFoundError(`Group ${groupId} does not exist!`);
+        throw new NotFoundError(`Group ${groupId} does not exist!`);
     }
-    if (!group.members.includes(userId) ) {
-      throw new NotAllowedError(`User ${userId}, not allowed to invite to ${groupId}`);
+
+    // Check if the user is a member using .equals() on ObjectId
+    const isMember = group.members.some(member => member.equals(userId));
+    if (!isMember) {
+        throw new NotAllowedError(`User ${userId} is not allowed to access group ${groupId}`);
     }
-  }
+}
 
   /**
    * Allows a user to leave the group voluntarily.
    */
+  // async leaveGroup(groupId: ObjectId, user: ObjectId) {
+  //   const group = await this.groups.readOne({ _id: groupId });
+  //   if (!group) throw new NotFoundError("Group not found!");
+
+  //   if (!group.members.includes(user)) {
+  //     throw new NotFoundError("You are not a member of this group!");
+  //   }
+
+  //   // Remove the user from members
+  //   group.members = group.members.filter(member => member.toString() !== user.toString());
+
+  //   await this.groups.partialUpdateOne({ _id: groupId }, { members: group.members });
+  //   return { msg: "You have successfully left the group!" };
+  // }
+
   async leaveGroup(groupId: ObjectId, user: ObjectId) {
     const group = await this.groups.readOne({ _id: groupId });
     if (!group) throw new NotFoundError("Group not found!");
 
-    if (!group.members.includes(user)) {
-      throw new NotFoundError("You are not a member of this group!");
+    // Check for membership using .equals() on ObjectId
+    const isMember = group.members.some(member => member.equals(user));
+    if (!isMember) {
+        throw new NotAllowedError(`User ${user}, not allowed to invite to ${groupId}`);
     }
 
     // Remove the user from members
-    group.members = group.members.filter(member => member.toString() !== user.toString());
+    group.members = group.members.filter(member => !member.equals(user));
 
     await this.groups.partialUpdateOne({ _id: groupId }, { members: group.members });
     return { msg: "You have successfully left the group!" };
-  }
+}
+
   }
